dee9f04a793f56ddf8e39a3cdf2b19b03421627c
HannaBusko <56487177+HannaBusko@users.noreply.github.com>
2020-02-08 22:15:01 +0300

    fix: fix error in checkCard

--- scripts/game.js
+++ scripts/game.js
@@ -46,7 +46,7 @@ function checkCard() {
     constantVariables.openedBigCards.forEach(value => {
         if (value.id == constantVariables.smallOpenedCard.attributes["data-img"].value) {
             if (value.cardDeckNumber == 0) {
-                deleteChild();
+                deleteChild("firstCardDeck");
 
                 /* let openedCard = { "cardDeckNumber": 0, "id": document.getElementById("parent").lastChild.alt};
                  openedBigCards.push(openedCard);*/
