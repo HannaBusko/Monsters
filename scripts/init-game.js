2c5a24cb5e14d92fcda6b07e82d20cccdb62f58a
HannaBusko <56487177+HannaBusko@users.noreply.github.com>
2020-02-16 17:11:46 +0300

    feat: change listeners

--- scripts/init-game.js
+++ scripts/init-game.js
@@ -40,15 +40,6 @@ var constantVariables = {
     "isGameOld": false
 }
 
-/*var gameplay = class Gameplay {
-    isPlaying() {
-        return false;
-    };
-    stopGame() {
-        return true;
-    }
-}*/
-
 function compareRandom(a, b) {
     return Math.random() - 0.5;
 }
@@ -56,14 +47,11 @@ function compareRandom(a, b) {
 function initGame() {
     constantVariables.smallOpenedCard = "";
     constantVariables.hasOpenedCard = false;
+    addHelpListeners();
     checkFinalCounterImg();
     sortBigCards(loadBigCards());
     sortSmallCards(loadSmallCards());
-    document.addEventListener('keydown', function (event) {
-        if (event.code == 'KeyH' && event.altKey) {
-            openForHelp();
-        }
-    });
+
 }
 
 function cleanCardField() {
@@ -82,4 +70,57 @@ function returnFirstPosition() {
     document.getElementById("thirdFinalPart").style.display = 'none';
     document.getElementById("loss").style.display = 'none';
     document.getElementById("win").style.display = 'none';
+    removeHelpListeners();
+}
+
+function checkKeyboard(EO) {
+    EO = EO || window.event;
+    if (EO.code == 'KeyH' && EO.altKey) {
+        openForHelp();
+    }
+}
+
+function addHelpListeners() {
+    document.addEventListener('keydown', checkKeyboard);
+
+    let longTouchTimer = "";
+    let monsterField = document.getElementById("monsterField");
+    monsterField.addEventListener('touchstart', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        longTouchTimer = setTimeout(openForHelp, 800);
+    }, false);
+
+    monsterField.addEventListener('touchmove', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        clearTimeout(longTouchTimer);
+    }, false);
+
+    monsterField.addEventListener('touchend', function (event) {
+        clearTimeout(longTouchTimer);
+    }, false);
+}
+
+function removeHelpListeners(){
+
+    document.removeEventListener('keydown', checkKeyboard);
+
+    let longTouchTimer = "";
+    let monsterField = document.getElementById("monsterField");
+    monsterField.removeEventListener('touchstart', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        longTouchTimer = setTimeout(openForHelp, 800);
+    }, false);
+
+    monsterField.removeEventListener('touchmove', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        clearTimeout(longTouchTimer);
+    }, false);
+
+    monsterField.removeEventListener('touchend', function (event) {
+        clearTimeout(longTouchTimer);
+    }, false);
 }
